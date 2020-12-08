b9c82a24b81e66242755204f6925669f91f6c56c
Evan Mobley <emmobley63@gmail.com>
2020-12-08 15:37:03 -0500

    ,Fixed the issue of avg change not having it's last parenthesis. It was causing syntax errors in print. The avg_change per month is off but the max is correct.

--- PyBank/main.py
+++ PyBank/main.py
@@ -13,32 +13,43 @@ with open(data_path) as datafile:
     pl_list = [] #Profit Loss numbers without the months
     date = []   #Months and year list
     mtm_change = [] #change in profit and loss without the months. Should be able to apply this to a list of months skipping the first month
-    z = 1
+    monthly_change = {} #creating a dict for the monthl: change in profit
     
 
+# Counting the months, adding the profit and Loss together
+# separating the dates and P/L into their own lists
     for x in csvreader:
         months_count += 1
         npl += float(x[1])
-        
         date.append(x[0])
         pl_list.append(float(x[1]))
+
+# Creating a change in profit list (it will have 1 less month than the month index
+# because we cannot calculate the change for the first month)
 
     for y in range(1, (len(pl_list)-1)):
         change = pl_list[y] - pl_list[y-1]
         mtm_change.append(change)  
     
-    
-    # for y in pl_list:
-    #     change = y[z]-y[z-1]
-    #     mtm_change.append(change)
-    #     z += 1
+#Need to creat a dictionary that pairs the months back together with 
+#the respective change in profit/loss change mtm
 
+
+    avg_change = ((sum(mtm_change))/(len(mtm_change)))
+    max_change = max(mtm_change)
 
     print(f"The total number of months in the data set is {months_count}.")
         
     print(f"The net total amount of Profit/Losses is ${npl}.")
 
-    print(f"{mtm_change}")    
     
+    print(f"Average Change: ${avg_change}")    
+    print(f"{max_change}")  
     
-
+#This was the original way I thought to make the loop
+#but this is accessing each item in the list and trying 
+#to index these float items
+# for y in pl_list:
+#     change = y[z]-y[z-1]
+#     mtm_change.append(change)
+#     z += 1
