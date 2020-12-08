d1a0874afb00f8a49b78ec5618a25e8bd11a4fbf
Evan Mobley <emmobley63@gmail.com>
2020-12-08 14:21:25 -0500

    ,Solved the month to month change for loop by using the variable y in the for loop to index the pl_list. THANK GODDDD

--- PyBank/main.py
+++ PyBank/main.py
@@ -10,8 +10,9 @@ with open(data_path) as datafile:
 
     months_count = int()
     npl = float()
-    pl_list = []
-    mtm_change = []
+    pl_list = [] #Profit Loss numbers without the months
+    date = []   #Months and year list
+    mtm_change = [] #change in profit and loss without the months. Should be able to apply this to a list of months skipping the first month
     z = 1
     
 
@@ -19,17 +20,25 @@ with open(data_path) as datafile:
         months_count += 1
         npl += float(x[1])
         
+        date.append(x[0])
         pl_list.append(float(x[1]))
-        
-    for y in pl_list:
-        mtm_change.append(float(y[z]-y[z-1])) 
+
+    for y in range(1, (len(pl_list)-1)):
+        change = pl_list[y] - pl_list[y-1]
+        mtm_change.append(change)  
+    
+    
+    # for y in pl_list:
+    #     change = y[z]-y[z-1]
+    #     mtm_change.append(change)
+    #     z += 1
 
 
     print(f"The total number of months in the data set is {months_count}.")
         
     print(f"The net total amount of Profit/Losses is ${npl}.")
 
-    print(f"{change_list}")    
+    print(f"{mtm_change}")    
     
     
 
