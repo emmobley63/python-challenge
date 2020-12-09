314f5de442b2d176cd8b0cd73a1e39f9712fe754
Evan Mobley <emmobley63@gmail.com>
2020-12-09 12:19:31 -0500

    ,Everything solved correctly. Mindie helped clear up the indexing of mtm_change and monthly_change.

--- PyBank/main.py
+++ PyBank/main.py
@@ -10,11 +10,15 @@ with open(data_path) as datafile:
 
     months_count = int()
     npl = float()
-    pl_list = [] #Profit Loss numbers without the months
-    date = []   #Months and year list
-    mtm_change = [] #change in profit and loss without the months. Should be able to apply this to a list of months skipping the first month
-    monthly_change = {} #creating a dict for the monthl: change in profit
-    
+    date = []           # Months and year list
+    mtm_change = []     # change in profit and loss without the months. Should be able to apply this to a list of months skipping the first month
+    monthly_change = [] # creating a dict for the monthl: change in profit
+    monthskip= []
+
+    first_row = next(csvreader)
+    prev_month = float(first_row[1])
+    months_count += 1
+    npl = prev_month
 
 # Counting the months, adding the profit and Loss together
 # separating the dates and P/L into their own lists
@@ -22,34 +26,46 @@ with open(data_path) as datafile:
         months_count += 1
         npl += float(x[1])
         date.append(x[0])
-        pl_list.append(float(x[1]))
+        change = float(x[1]) - prev_month
+        prev_month = float(x[1])
+        mtm_change.append(change)
+        monthly_change.append(x[0])
 
-# Creating a change in profit list (it will have 1 less month than the month index
+# Creating a change in profit list (it will have 1 less month than the datelist
 # because we cannot calculate the change for the first month)
 
-    for y in range(1, (len(pl_list)-1)):
-        change = pl_list[y] - pl_list[y-1]
-        mtm_change.append(change)  
+
     
 #Need to creat a dictionary that pairs the months back together with 
 #the respective change in profit/loss change mtm
+    
 
-
+    max_index = mtm_change.index(max(mtm_change))
+    min_index = mtm_change.index(min(mtm_change)) 
+    
     avg_change = ((sum(mtm_change))/(len(mtm_change)))
-    max_change = max(mtm_change)
 
-    print(f"The total number of months in the data set is {months_count}.")
-        
-    print(f"The net total amount of Profit/Losses is ${npl}.")
+#Instead of a dictionary. Trying to zip the different lists together and then if statement to print when index[1]=max,min
+# or set variable to date.pop(0)
 
-    
+
+
+# check and make sure this works before asking. might have to f format
+#the max_month to str and index row    
+    # for row in monthchange_list:
+    #     if row[1] == max_change:
+    #         max_month = row
+    #     if row[1] == min_change:
+    #         min_month = row
+
+    print("----------------------------------------------------------------")
+    print("Financial Analysis")
+    print("--------------------------------------")
+    print(f"Number of Months: {months_count}")
+    print(f"Net Total Profit/Loss: ${npl}")
     print(f"Average Change: ${avg_change}")    
-    print(f"{max_change}")  
+    print(f'Greatest Increase in Profits: {monthly_change[max_index]} ${mtm_change[max_index]}')
+    print(f'Greatest Decrease in Profits: {monthly_change[min_index]} ${mtm_change[min_index]}')
+    print("----------------------------------------------------------------")
     
-#This was the original way I thought to make the loop
-#but this is accessing each item in the list and trying 
-#to index these float items
-# for y in pl_list:
-#     change = y[z]-y[z-1]
-#     mtm_change.append(change)
-#     z += 1
+
