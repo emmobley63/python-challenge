f8d639912fd9d7f9d0cc8dc68d5f56ba6935211e
Evan Mobley <emmobley63@gmail.com>
2020-12-09 13:35:16 -0500

    ,Forgot to save both PyBank and Pypoll

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -23,31 +23,46 @@ with open(data_path) as datafile:
         county.append(x[1])
         candidate.append(x[2])
         if x[2] == "Li":
-            Lcount +=1
+            Lcount += 1
         if x[2] == "Khan":
-            Kcount +=1
+            Kcount += 1
         if x[2] == "Correy":
-            Ccount +=1
+            Ccount += 1
         if x[2] == "O'Tooley":
-            Ocount +=1
+            Ocount += 1
     
     candidate_set = (set(candidate))
     kper = float(Kcount/tv) *100
     cper = float(Ccount/tv) *100
     lper = float(Lcount/tv) *100
     oper = float(Ocount/tv) *100
-    winner = str()
+    cancount = {"Khan": kper, 
+                "Correy": cper, 
+                "Li": lper, 
+                "O'Tooley": oper}
+    
+    # for key, value in cancount.items():
+        
+
+
+    # winner = "O'Tooley"
+    # if kper > 50 or (kper > oper)
+    #     winner = "Khan"
+    # if cper > 50 or ()
+    #     winner = "Correy"
+    # if lper > 50
+    #     winner = "Li"
     
 
 
-    print('Election Results') 
-    print('-----------------------')
-    print(f'Total Votes: {tv}')
-    print('-----------------------')
-    print(f'Khan: {kper}% ({Kcount})')
-    print(f'Correy: {cper}% ({Ccount})')
-    print(f'Li: {lper}% ({Lcount})')
+    print("Election Results") 
+    print("-----------------------")
+    print(f"Total Votes: {tv}")
+    print("-----------------------")
+    print(f"Khan: {kper}% ({Kcount})")
+    print(f"Correy: {cper}% ({Ccount})")
+    print(f"Li: {lper}% ({Lcount})")
     print(f"O'Tooley: {oper}% ({Ocount})")
-    print('-----------------------')
-    print(f"Winner: ")
-    print('-----------------------')
+    print("-----------------------")
+    print(f"Winner: {winner}")
+    print("-----------------------")
